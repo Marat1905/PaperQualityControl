b79cfdbb0055b0b2e742ef39a27a0cff4ae2ec24
Marat <milya010790@mail.ru>
2025-09-01 15:50:35 +0500

    Добавление шняги всякой

--- PaperQualityControl/ViewModels/BaseViewModel.cs
+++ PaperQualityControl/ViewModels/BaseViewModel.cs
@@ -1,12 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace PaperQualityControl.ViewModels
 {
-    internal class BaseViewModel
+    public class BaseViewModel : INotifyPropertyChanged, IAsyncDisposable
     {
+        private string _statusMessage = "Ready";
+        private DateTime _timestamp = DateTime.Now;
+        private bool _isBusy;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public string StatusMessage
+        {
+            get => _statusMessage;
+            set => SetField(ref _statusMessage, value);
+        }
+
+        public DateTime Timestamp
+        {
+            get => _timestamp;
+            set => SetField(ref _timestamp, value);
+        }
+
+        public bool IsBusy
+        {
+            get => _isBusy;
+            set => SetField(ref _isBusy, value);
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        public virtual ValueTask DisposeAsync()
+        {
+            return ValueTask.CompletedTask;
+        }
     }
 }
